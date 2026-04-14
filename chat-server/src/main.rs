e18eadc4770fd23b057f61fdbabb486f7d3fea28
Gor <gor.hakobyan97@gmail.com>
2026-04-14 23:02:30 +0200

    Basic TCP connection estblished
    Established basic TCP connection using TcpStream. By doing cargo -run in one terminal and connecting from another using telnet (telnet 127.0.0.1 9000), I can write messages in client and see it in server and see how it echos back to the client. Very raw but we have connection!

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+
+const PORT_ADDRESS: &str = "0.0.0.0:9000";
+
+fn handle_client(stream: TcpStream) {
+    if let Ok(addr) = stream.peer_addr() {
+        println!("Client connected: {addr}");
+
+        let mut writer = stream.try_clone().expect("Failed to clone stream");
+        // BufReader for reading line by line instead of raw bytes
+        let reader = BufReader::new(stream);
+
+        for line in reader.lines() {
+            match line {
+                Err(_) => {
+                    println!("Client disconnected");
+                    break;
+                }
+                Ok(txt) => {
+                    println!("[{addr}] received: {txt}");
+                    let response = format!("Message received: {txt}\n");
+                    match writer.write_all(response.as_bytes()) {
+                        Err(e) => println!("Error writing bytes: {e}"),
+                        Ok(_) => {}
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind(PORT_ADDRESS)?;
+    println!("Listening to {PORT_ADDRESS}");
+
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => eprintln!("Connection to {PORT_ADDRESS} failed with error {e}"),
+            Ok(stream) => handle_client(stream),
+        }
+    }
+
+    Ok(())
 }
